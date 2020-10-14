ee7dfda934fda728017cd9c93aeaa3597b9efd0a
LirysJH <48212972+LirysJH@users.noreply.github.com>
2020-10-14 23:23:16 +0300

    built Title component

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './css/Title.css';
+
+export const Title = (props) => {
+
+        
+    return (
+        <div className="Title">
+            <div className="block">
+                <h1 className="title">
+                    Pokedex
+                </h1>
+            </div>
+        </div>
+    )
+}
