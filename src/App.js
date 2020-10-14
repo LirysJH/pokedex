c0f127f39c4791c1b0afdd0b2cba3e2d03261e41
LirysJH <48212972+LirysJH@users.noreply.github.com>
2020-10-15 00:12:56 +0300

    imported components; added styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Description } from './components/Description/Description';
+import { Pokemons } from './components/Pokemons/Pokemons';
+import { Title } from './components/Title/Title';
 import './css/App.css';
 
 export default class App extends Component {
@@ -6,7 +9,11 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        App
+        <Title />
+        <div className="block">
+          <Pokemons />
+          <Description />
+        </div>
       </div>
     );
   }
