0fae89e76fbf39bf84266cd1be2aceaa92add66d
LirysJH <48212972+LirysJH@users.noreply.github.com>
2020-10-15 19:47:18 +0300

    assigned pokemons data fetching and mapping

--- src/components/Pokemons/Pokemons.jsx
+++ src/components/Pokemons/Pokemons.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/Pokemons.css';
 
 export const Pokemons = (props) => {
 
+    // const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
+    const API_URL = `http://pokeapi.co/api/v2/pokemon/?limit=12`;
+
+    const [pokemons, setPokemons] = useState([]);
+
+    useEffect(() => {
+        loadData();
+    }, []);
+  
+    const loadData = async () => {
+        const response = await fetch(API_URL);
+        // const response = await fetch(PROXY_URL + API_URL);
+        const data = await response.json();
+        setPokemons(data.results);
+    }
         
     return (
         <div className="Pokemons">
-            Pokemons
-            <br/>
-            Pokemons
-            <br/>
-            Pokemons
-            <br/>
-            Pokemons
-            <br/>
-            Pokemons
-            <br/>
-            Pokemons
-            <br/>
+            {
+                pokemons.map((p, id) => (
+                    <div className="box" key={id}>
+                        { p.name }
+                    </div>
+                ))
+            }
         </div>
     )
 }
