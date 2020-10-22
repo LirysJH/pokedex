d2a943150a47dc1ec1bb0c45c4181a4efb84194f
LirysJH <48212972+LirysJH@users.noreply.github.com>
2020-10-23 01:48:26 +0300

    implemented fetch and set pokemons data

--- src/components/Pokemons/Pokemons.jsx
+++ src/components/Pokemons/Pokemons.jsx
@@ -7,26 +7,93 @@ export const Pokemons = (props) => {
     const API_URL = `http://pokeapi.co/api/v2/pokemon/?limit=12`;
 
     const [pokemons, setPokemons] = useState([]);
+    const [pokemonsData, setPokemonsData] = useState([]);
+    const [pokemonsTypes, setPokemonsTypes] = useState([]);
 
     useEffect(() => {
-        loadData();
+        loadSetData();
     }, []);
   
-    const loadData = async () => {
+    const loadSetData = async () => {
         const response = await fetch(API_URL);
         // const response = await fetch(PROXY_URL + API_URL);
         const data = await response.json();
         setPokemons(data.results);
+
+        // get data for each pokemon via url
+
+        let responsePokeData = {},
+            responseObject= {},
+            pokeData = [];
+        
+        await Promise.all(            
+            data.results.map(async el => {
+                responsePokeData = await fetch(el.url);
+                responseObject = await responsePokeData.json();
+                pokeData.push(responseObject);
+            })
+        );
+
+        setPokemonsData(pokeData);
+
+        // get all types for each pokemon
+
+        let pokemonTypes = [],
+        allTypes = [];
+        
+        pokeData.map(el => {
+            for(let type of el.types)
+            {
+                pokemonTypes.push(type.type.name);
+            }
+            allTypes.push(pokemonTypes);
+            pokemonTypes = [];
+        });
+
+        allTypes = buildTypesBlock(allTypes);        
+        setPokemonsTypes(allTypes);
+    }
+
+    const buildTypesBlock = (arr) => {
+        let items = [],
+            itemSet = [];
+        
+        arr.forEach( element => {
+            element.forEach( el => {
+                itemSet.push(
+                    <div className="container__block">
+                        { el }
+                    </div>
+                ); 
+            })
+            
+            items.push(itemSet);
+            itemSet = [];
+        })
+
+        return items;
     }
         
     return (
         <div className="Pokemons">
             {
-                pokemons.map((p, id) => (
+                pokemonsData.map((p, id) => (
                     <div className="box" key={id}>
+                        <img
+                            src={ p.sprites.front_default }
+                            alt="pokemon-img"
+                            title={ p.name }
+                            className="icon"
+                        />
                         { p.name }
+                        <div className="container">
+                            { pokemonsTypes[id] }
+                        </div>
                     </div>
                 ))
+            }
+            {
+
             }
         </div>
     )
