8960de8a753cd525c8c00396691205aa145fd9ed
LirysJH <48212972+LirysJH@users.noreply.github.com>
2020-10-27 01:04:42 +0200

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Installation
-
-In the project directory, you can run:
-
-### `npm install`
-
-Installs all dependencies
---
+++ precache-manifest.ad73e5fc61b3ecf298febe023a13d928.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b57f4a41272415abaef6b654402050",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "838ce518314fdaf269a0",
+    "url": "/pokedex/static/css/main.6997a6ac.chunk.css"
+  },
+  {
+    "revision": "facd17e988c5923f6aae",
+    "url": "/pokedex/static/js/2.f6f048b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.f6f048b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "838ce518314fdaf269a0",
+    "url": "/pokedex/static/js/main.22087fc4.chunk.js"
+  },
+  {
+    "revision": "388f63e3668b7d1dba87",
+    "url": "/pokedex/static/js/runtime-main.f558bce3.js"
+  }
+]);
